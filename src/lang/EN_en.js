0f1722fc2ad04cb338d41c6732ae6abf2322a24c
Saskia Schreiber <sas.schreiber@gmail.com>
2015-05-26 19:58:55 +0200

    [TASK] Fix missing dependencies

---
+++ src/lang/EN_en.js
@@ -0,0 +1,10 @@
+App.Lang = {
+	NEXT_BUTTON : "Next",
+	PREV_BUTTON : "Previous",
+	FINISH_BUTTON : "Finish",
+	CLOSE_BUTTON : "Close",
+
+	ERROR_VISIT_PREVIOUS_TITLE : "Navigation error!",
+	ERROR_MSG_VISIT_PREV : "You cannot visit the next page before finishing the current page!",
+	ERROR_MSG_WRONG_ANSWER : "Your answers are not correct! Please correct them before continuing!"
+}
