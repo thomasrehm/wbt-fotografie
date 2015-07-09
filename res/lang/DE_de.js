9e102bbda4ad943a1b21320625e412c07658b6d6
Saskia Schreiber <sas.schreiber@gmail.com>
2015-07-09 12:03:44 +0200

    [FEATURE] Include custom language file

---
+++ res/lang/DE_de.js
@@ -0,0 +1,11 @@
+App.Lang = {
+	PREV_BUTTON : "Zurück",
+	NEXT_BUTTON : "Weiter",
+	FINISH_BUTTON : "Abschließen",
+	CLOSE_BUTTON : "Schließen",
+
+
+	ERROR_VISIT_PREVIOUS_TITLE : "Navigations-Fehler!",
+	ERROR_MSG_VISIT_PREV : "Sie können diesen Abschnitt noch nicht besuchen, da Sie die vorherigen Abschnitte noch nicht abgeschlossen haben!",
+	ERROR_MSG_WRONG_ANSWER : "Schade, leider hast du nicht alles richtig beantwortet. Schau dir deine Fehler genau an. Hast du etwas nicht richtig verstanden, beantworte die Fragen anhand der Hinweise und lese den entsprechenden Abschnitt erneut."
+}
